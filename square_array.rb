e5bb7858ea16d3c467706a890deb1a0e24987ce6
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-06 23:16:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 def square_array(array)
     new_array = []
     array.each { |e| new_array << e ** 2 }
     new_array
 end
+=======
+def square_array(array)
+    new_array = []
+    array.each { |e| new_array << e ** 2 }
+    new_array
+end
+>>>>>>> 60abfe496311fecdc8ab05d93b48b3e2af97b260
